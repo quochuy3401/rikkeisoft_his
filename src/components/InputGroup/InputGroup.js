a6ca67a4bb278fb61ef3d14258db11ecb1b86ee2
Hoàng Quốc Huy <hoangquochuy3401@gmail.com>
2022-05-05 16:38:08 +0700

    change props fields of InputGroup

--- src/components/InputGroup/InputGroup.js
+++ src/components/InputGroup/InputGroup.js
@@ -2,7 +2,7 @@ import { Col, Row } from "react-bootstrap";
 import "./InputGroup.css";
 
 export const InputGroup = (props) => {
-  const { label, id, name } = props;
+  const { label, id, name, disabled } = props;
   return (
     <>
       <div className="input-wrapper d-flex">
@@ -10,7 +10,7 @@ export const InputGroup = (props) => {
           <label>{label}</label>
         </div>
         <div>
-          <input type="text" id={id} name={name} />
+          <input type="text" id={id} name={name} disabled={disabled || false} />
         </div>
       </div>
     </>
