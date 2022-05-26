0ba4bfeaf0495baaf7984fa76797243c86510c5e
bullpig <duycuong565@gmail.com>
2022-05-26 16:25:29 +0700

    fix navbar

--- src/components/PatientList/PatientList.js
+++ src/components/PatientList/PatientList.js
@@ -9,7 +9,7 @@ export default function PatientList() {
     const [contacts, setContacts] = useState(MOCK_DATA)
     const [searchColumns, setSearchColumns] = useState(searchObjects)
     const [q, setQ] = useState('')
-    
+
     function search(rows) {
         return rows.filter((row) =>
             searchColumns.some(
@@ -17,52 +17,52 @@ export default function PatientList() {
             )
         )
     }
-   
+
     return (
         <div className='container-fluid patient-table-container'>
             <div className='filter'>
                 <input type="text" value={q} onChange={e => setQ(e.target.value)} placeholder='Search...' />
                 {
-                    searchObjects.map((column) => 
-                    <label>
-                        <input type="checkbox" checked={searchColumns.includes(column)} 
-                            onChange={(e) => {
-                                const checked = searchColumns.includes(column)
-                                setSearchColumns(prev => checked ? prev.filter(sc => sc !== column) : [...prev, column])
-                            }}
-                        />
-                        {column}
-                    </label>)
+                    searchObjects.map((column) =>
+                        <label>
+                            <input type="checkbox" checked={searchColumns.includes(column)}
+                                onChange={(e) => {
+                                    const checked = searchColumns.includes(column)
+                                    setSearchColumns(prev => checked ? prev.filter(sc => sc !== column) : [...prev, column])
+                                }}
+                            />
+                            {column}
+                        </label>)
                 }
             </div>
-           <table>
-               <thead>
-                <tr>
-                    <th>Số hồ sơ</th>
-                    <th>Tên bệnh nhân</th>
-                    <th>Tuổi</th>
-                    <th>Giới tính</th>
-                    <th>Địa chỉ</th>
-                    <th>Ngày khám</th>
-                    <th>Trạng thái hồ sơ</th>
-                    <th>Đối tượng</th>
-                </tr>
-               </thead>
-               <tbody className="table-patient">
-                {search(contacts).map((contact) => (
-                    <tr key={contact.file_number}>
-                        <td>{contact.file_number}</td>
-                        <td>{contact.full_name}</td>
-                        <td>{contact.age}</td>
-                        <td>{contact.gender}</td>
-                        <td>{contact.address}</td>
-                        <td>{contact.examine_date}</td>
-                        <td><input type="checkbox" checked={contact.file_status ? 'checked' : ''} onChange={e => {}}/></td>
-                        <td>{contact.patient_type}</td>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Số hồ sơ</th>
+                        <th>Tên bệnh nhân</th>
+                        <th>Tuổi</th>
+                        <th>Giới tính</th>
+                        <th>Địa chỉ</th>
+                        <th>Ngày khám</th>
+                        <th>Trạng thái hồ sơ</th>
+                        <th>Đối tượng</th>
                     </tr>
-                ))}
-               </tbody>
-           </table>
+                </thead>
+                <tbody className="table-patient">
+                    {search(contacts).map((contact) => (
+                        <tr key={contact.file_number}>
+                            <td>{contact.file_number}</td>
+                            <td>{contact.full_name}</td>
+                            <td>{contact.age}</td>
+                            <td>{contact.gender}</td>
+                            <td>{contact.address}</td>
+                            <td>{contact.examine_date}</td>
+                            <td><input type="checkbox" checked={contact.file_status ? 'checked' : ''} onChange={e => { }} /></td>
+                            <td>{contact.patient_type}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
